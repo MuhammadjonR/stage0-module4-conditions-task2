dc779ee29c6d3cb511b3887d79390d168ac9e7ce
MuhammadjonR <100756194+MuhammadjonR@users.noreply.github.com>
2022-07-17 02:00:32 +0500

    Update GreatestNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
-    }
+    first=5;
+    second=8;
+    if(first > second){
+System.out.println(first);
+}else{
+System.out.println(second)
+}
+}
 }
